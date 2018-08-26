2d7ae12bf2918352d4cb8f359ad6108ae52fcefc
SongSa8818 <kimsong.kg@gmail.com>
2018-08-26 19:46:15 +0700

    W-20 : As developer, I want to create sangkat in parameters, so that user can select sangkat.

---
+++ app/Http/Controllers/SangkatController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Khan;
+use App\Sangkat;
+use Illuminate\Http\Request;
+
+class SangkatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $sangkat = Sangkat::SangkatKhan();
+      return view('admin.parameters.sangkat')->with('sangkats', $sangkat);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      $khan = Khan::pluck('name', 'id');
+      return view('admin.parameters.formSangkat')->with('khans', $khan);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $sangkat = new Sangkat();
+      $sangkat->name = $request->name;
+      $sangkat->khan_id = $request->khan_id;
+      $sangkat->save();
+      return redirect('sangkat');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Sangkat  $sangkat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sangkat $sangkat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Sangkat  $sangkat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      $sangkat = Sangkat::findOrFail($id);
+      $khan = Khan::pluck('name', 'id');
+      return view('admin.parameters.formSangkat')->with(array('sangkat' => $sangkat, 'khans' => $khan));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Sangkat  $sangkat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $sangkat = Sangkat::findOrFail($id);
+      $sangkat->name = $request->name;
+      $sangkat->khan_id = $request->khan_id;
+      $sangkat->save();
+      return redirect('sangkat');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Sangkat  $sangkat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      Sangkat::destroy($id);
+      return redirect('sangkat');
+    }
+}
