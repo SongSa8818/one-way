7ffbf316c4047430025370a8e86abefbc21ba948
SongSa8818 <kimsong.kg@gmail.com>
2018-09-07 20:03:55 +0700

    Master updated

--- app/Active.php
+++ app/Active.php
@@ -6,6 +6,10 @@ use Enum;
 
 abstract class Active extends enum
 {
-  const ACTIVE = 'active';
-  const INACTIVE = 'inactive';
+    const ACTIVE = 'Active';
+    const INACTIVE = 'Inactive';
+    const SHOWING = 'Showing';
+    const OFFERING = 'Offering';
+    const REQUESTED = 'Requested';
+    const REJECT_OFFER = 'Requested';
 }
