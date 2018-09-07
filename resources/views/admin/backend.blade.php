92d59c2431bd78da58818f6b1b57f3e1eb310316
SongSa8818 <kimsong.kg@gmail.com>
2018-09-07 23:00:44 +0700

    W-33 : As admin, I want to update menu & dashboard backend page.

--- resources/views/admin/backend.blade.php
+++ resources/views/admin/backend.blade.php
@@ -119,27 +119,43 @@
                 </div>
                 <ul class="sidebar-menu">
                     <li class="menu-header">Dashboard</li>
+                    <li>
+                        <a href="{{ route('home') }}"><i class="ion ion-wifi"></i><span>Visit Site</span></a>
+                    </li>
                     <li class="{{ $uri == 'dashboard.index'?'active':'' }}">
                         <a href="{{ route('dashboard.index') }}"><i class="ion ion-speedometer"></i><span>Dashboard</span></a>
                     </li>
                     <li class="{{ in_array($uri, array('property.list','property.create','image.edit','image.create'))?'active':'' }}">
-                        <a href="{{ route('property.list') }}"><i class="ion ion-home"></i><span>Properties</span></a>
+                        <a class="has-dropdown" href=""><i class="ion ion-home"></i><span>Properties</span></a>
+                        <ul class="menu-dropdown">
+                            <li class="">
+                                <a href="{{ route('property.list') }}"><i class="ion ion-ios-home-outline"></i><span>All</span></a>
+                            </li>
+                            <li class="">
+                                <a href=""><i class="ion ion-social-usd"></i><span>Offered</span></a>
+                            </li>
+                            <li class="">
+                                <a href=""><i class="ion ion-eye"></i><span>Showing</span></a>
+                            </li>
+                        </ul>
                     </li>
                     <li class="{{ in_array($uri, array('user.index','user.edit'))?'active':'' }}">
-                        <a href="{{ route('user.index') }}"><i class="ion ion ion-ios-contact-outline"></i><span>Users</span></a>
+                        <a href="{{ route('user.index') }}"><i class="ion ion-android-contact"></i><span>Agency</span></a>
                     </li>
                     <li class="{{ in_array($uri, array('customer.list','customer.edit'))?'active':'' }}">
-                        <a href="{{ route('customer.list') }}"><i class="ion ion-ios-person-outline"></i><span>Customers</span></a>
-                    </li>
-                    <li class="{{ in_array($uri, array('/contact/1/edit','contact'))?'active':'' }}">
-                        <a href="{{ route('contact.edit', 1) }}"><i class="ion ion-ios-contact-outline"></i><span>Contact Info</span></a>
+                        <a href="{{ route('customer.list') }}"><i class="ion ion-person-stalker"></i><span>Customers</span></a>
                     </li>
                     <li class="{{ in_array($uri, array('request-list','request'))?'active':'' }}">
                         <a href="{{ route('request.list') }}"><i class="ion ion-email-unread"></i><span>Request Info</span></a>
                     </li>
-                    <li>
+                    <li class="{{ in_array($uri, array('/contact/1/edit','contact'))?'active':'' }}">
+                        <a href="{{ route('contact.edit', 1) }}"><i class="ion ion-ios-telephone"></i><span>Contact us</span></a>
+                    </li>
                     <li class="{{ in_array($uri, array('/about/1/edit','about'))?'active':'' }}">
-                        <a href="{{ route('about.edit', 1) }}"><i class="ion ion-clipboard"></i><span>About</span></a>
+                        <a href="{{ route('about.edit', 1) }}"><i class="ion ion-clipboard"></i><span>About us</span></a>
+                    </li>
+                    <li class="{{ in_array($uri, array('user.index','user.edit'))?'active':'' }}">
+                        <a href="{{ route('user.index') }}"><i class="ion ion-ios-contact-outline"></i><span>Users</span></a>
                     </li>
                     <li class="{{ in_array($uri,array(
                             'city.index','city.create','city.edit',
@@ -155,76 +171,7 @@
                             <li class="{{ in_array($uri,array('village.index','village.create','village.edit'))?'active':'' }}"><a href="{{ route('village.index') }}"><i class="ion ion-ios-location-outline"></i> Village</a></li>
                         </ul>
                     </li>
-                    <li class="menu-header">Components</li>
-                    <li>
-                        <a href="#" class="has-dropdown"><i class="ion ion-ios-albums-outline"></i><span>Components</span></a>
-                        <ul class="menu-dropdown">
-                            <li><a href="general.html"><i class="ion ion-ios-circle-outline"></i> Basic</a></li>
-                            <li><a href="components.html"><i class="ion ion-ios-circle-outline"></i> Main Components</a></li>
-                            <li><a href="buttons.html"><i class="ion ion-ios-circle-outline"></i> Buttons</a></li>
-                            <li><a href="toastr.html"><i class="ion ion-ios-circle-outline"></i> Toastr</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" class="has-dropdown"><i class="ion ion-flag"></i><span>Icons</span></a>
-                        <ul class="menu-dropdown">
-                            <li><a href="ion-icons.html"><i class="ion ion-ios-circle-outline"></i> Ion Icons</a></li>
-                            <li><a href="fontawesome.html"><i class="ion ion-ios-circle-outline"></i> Font Awesome</a></li>
-                            <li><a href="flag.html"><i class="ion ion-ios-circle-outline"></i> Flag</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="table.html"><i class="ion ion-clipboard"></i><span>Tables</span></a>
-                    </li>
-                    <li>
-                        <a href="chartjs.html"><i class="ion ion-stats-bars"></i><span>Chart.js</span></a>
-                    </li>
-                    <li>
-                        <a href="simple.html"><i class="ion ion-ios-location-outline"></i><span>Google Maps</span></a>
-                    </li>
-                    <li>
-                        <a href="#" class="has-dropdown"><i class="ion ion-ios-copy-outline"></i><span>Examples</span></a>
-                        <ul class="menu-dropdown">
-                            <li><a href="login.html"><i class="ion ion-ios-circle-outline"></i> Login</a></li>
-                            <li><a href="register.html"><i class="ion ion-ios-circle-outline"></i> Register</a></li>
-                            <li><a href="forgot.html"><i class="ion ion-ios-circle-outline"></i> Forgot Password</a></li>
-                            <li><a href="reset.html"><i class="ion ion-ios-circle-outline"></i> Reset Password</a></li>
-                            <li><a href="404.html"><i class="ion ion-ios-circle-outline"></i> 404</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="menu-header">More</li>
-                    <li>
-                        <a href="#" class="has-dropdown"><i class="ion ion-ios-nutrition"></i> Click Me</a>
-                        <ul class="menu-dropdown">
-                            <li><a href="#"><i class="ion ion-ios-circle-outline"></i> Menu 1</a></li>
-                            <li><a href="#" class="has-dropdown"><i class="ion ion-ios-circle-outline"></i> Menu 2</a>
-                                <ul class="menu-dropdown">
-                                    <li><a href="#"><i class="ion ion-ios-circle-outline"></i> Child Menu 1</a></li>
-                                    <li><a href="#"><i class="ion ion-ios-circle-outline"></i> Child Menu 2</a></li>
-                                    <li><a href="#" class="has-dropdown"><i class="ion ion-ios-circle-outline"></i> Child Menu 3</a>
-                                        <ul class="menu-dropdown">
-                                            <li><a href="#"><i class="ion ion-ios-circle-outline"></i> Child Menu 1</a></li>
-                                            <li><a href="#"><i class="ion ion-ios-circle-outline"></i> Child Menu 2</a></li>
-                                            <li><a href="#"><i class="ion ion-ios-circle-outline"></i> Child Menu 3</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="#"><i class="ion ion-ios-circle-outline"></i> Child Menu 4</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#"><i class="ion ion-heart"></i> Badges <div class="badge badge-primary">10</div></a>
-                    </li>
-                    <li>
-                        <a href="credits.html"><i class="ion ion-ios-information-outline"></i> Credits</a>
-                    </li>          </ul>
-                <div class="p-3 mt-4 mb-4">
-                    <a href="http://stisla.multinity.com/" class="btn btn-danger btn-shadow btn-round has-icon has-icon-nofloat btn-block">
-                        <i class="ion ion-help-buoy"></i> <div>Go PRO!</div>
-                    </a>
-                </div>
+                </ul>
             </aside>
         </div>
         <div class="main-content">
@@ -232,7 +179,7 @@
         </div>
         <footer class="main-footer">
             <div class="footer-left">
-                Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://multinity.com/">Multinity</a>
+                Copyright &copy; 2019 <div class="bullet"></div> Design By <a href="http://onewayrealtymanagement.com/">One Way Realty</a>
             </div>
             <div class="footer-right"></div>
         </footer>
