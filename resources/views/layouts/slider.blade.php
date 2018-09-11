45a9f8a8b5629520320ada029cdf369e1a1d2253
vaeha <vaehachun@gmail.com>
2018-09-11 21:33:05 +0700

    W-27 : As admin, I want to manage slideshow images in home page, so that I can update images later.

--- resources/views/layouts/slider.blade.php
+++ resources/views/layouts/slider.blade.php
@@ -1,46 +1,23 @@
-
 <!-- Home -->
 <div class="home">
     <!-- Home Slider -->
     <div class="home_slider_container">
         <div class="owl-carousel owl-theme home_slider">
+        @foreach($slideshows as $slideshow)
             <!-- Home Slider Item -->
-            <div class="owl-item home_slider_item">
-                <div class="owl-item home_slider_item">
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                        <!-- Carousel Slideshow -->
-                        <div id="carousel-example" class="carousel slideshow" data-ride="carousel">
-                            <!-- Carousel Indicators -->
-                            <ol class="carousel-indicators">
-                                <li data-target= src="{{ url('/uploads/slideshows/'.@$slideshow->image) }}" data-slideshow-to="0" class="active"></li>
-                                <li data-target= src="{{ url('/uploads/slideshows/'.@$slideshow->image) }}" data-slideshow-to="1"></li>
-                                <li data-target= src="{{ url('/uploads/slideshows/'.@$slideshow->image) }}" data-slideshow-to="2"></li>
-                                <li data-target= src="{{ url('/uploads/slideshows/'.@$slideshow->image) }}" data-slideshow-to="3"></li>
-                                <li data-target= src="{{ url('/uploads/slideshows/'.@$slideshow->image) }}" data-slideshow-to="4"></li>
-                            </ol>
-                            <div class="clearfix"></div>
-                            <!-- End Carousel Indicators -->
-                            <!-- Carousel Images -->
-                            <div class="carousel-inner">
-                                @foreach ($slideshows as $key => $slideshow)
-                                    <div class="item{{ $key == 0 ? ' active' : '' }}">
-                                        <img src="{{ $slideshow->image }}">
-                                    </div>
-                                @endforeach
-                            <!-- Carouse Images -->
-                                <!-- Carousel Controls -->
-                                <a class="left carousel-control" href="#carousel-example" data-slide="prev">
-                                    <span class="glyphicon glyphicon-chevron-left"></span>
-                                </a>
-                                <a class="right carousel-control" href="#carousel-example" data-slide="next">
-                                    <span class="glyphicon glyphicon-chevron-right"></span>
-                                </a>
-                                <!-- End Carousel Controls -->
-                            </div>
-                        </div>
-                    </div>
+            <div class="home_slider_background">
+                <!-- ImageProperty by https://unsplash.com/@aahubs -->
+                <div class="sidebar-user-picture">
+                    <img src="{{ url('/uploads/slideshows/'.@$slideshow->image) }}"/>
                 </div>
             </div>
+         @endforeach
         </div>
+
+        <!-- Home Slider Nav -->
+        <div class="home_slider_nav_left home_slider_nav d-flex flex-row align-items-center justify-content-end">
+            <img src="images/nav_left.png" alt="">
+        </div>
+
     </div>
 </div>
